cb72dd34cc4e28252ee00ac43b936cce85bc798f
Ayaan Faisal <ayaanfaisaall@gmail.com>
2026-04-19 16:59:03 +0500

    v1.0, d13.0

--- .config/hypr/scripts/toggleopaque.sh
+++ .config/hypr/scripts/toggleopaque.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-# Active window ka exact address nikalna
 address=$(hyprctl activewindow -j | jq -r '.address')
-
-# Error handling: Agar koi window focus mein na ho
+class=$(hyprctl activewindow -j | jq -r '.initialClass')
+ 
 if [ -z "$address" ] || [ "$address" == "null" ]; then
     exit 0
 fi
 
-# Ek temporary state file jo check karegi ke window is waqt solid hai ya nahi
 state_file="/tmp/opaque_$address"
 
 if [ -f "$state_file" ]; then
-    # Agar pehle se solid hai, toh file delete karein aur wapas transparent/blur kar dein
     rm "$state_file"
     hyprctl setprop active opaque 0
+    if [ $class == "kitty" ]; then
+	sed -i "s|0.8|1.0|g" ~/.config/kitty/kitty.conf
+        killall -USR1 kitty
+    fi
 else
-    # Agar transparent hai, toh state file create karein aur 100% solid kar dein
     touch "$state_file"
     hyprctl setprop active opaque 1
+    if [ $class == "kitty" ]; then
+	sed -i "s|1.0|0.8|g" ~/.config/kitty/kitty.conf
+        killall -USR1 kitty
+    fi
 fi
